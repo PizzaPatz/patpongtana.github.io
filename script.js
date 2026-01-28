6c996f3d393ea319926e1d7d11e3767bfc2ffd1e
acn_patrapee.pongtana <patrapee.pongtana@gmail.com>
2026-01-28 17:15:55 +0700

    init add homepage

---
+++ script.js
@@ -0,0 +1,8 @@
+// Mobile menu toggle
+const menuToggle = document.querySelector('.menu-toggle');
+const mainNav = document.querySelector('.main-nav');
+
+menuToggle.addEventListener('click', () => {
+  mainNav.classList.toggle('active');
+});
+
